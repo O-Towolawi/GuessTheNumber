de9b652c5f0f31704e1fb0f3950dedef8246e9c2
O-Towolawi <setowolawi3@gmail.com>
2023-03-04 01:35:48 +0000

    Update GuessTheNumber.py

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -48,44 +48,13 @@ def rules():
     print()
     menu()
 
-def scoreboard(guesses = 0, number = 0, guess = 0):
-    if guesses == 0 and number == 0 and guess == 0:
-        print("Scoreboard currently empty. Play then come back!")
-        print()
-        menu()
-    else:
-        scores = {}
-        user = input("Enter player name: ")
-        with open("scores.txt", "w") as file:
-            if number == guess:
-                file.write("{},{}".format(user, guesses))
-        with open("scores.txt", "r") as file:
-            for line in file.readlines():
-                l = line.strip.split(",")
-                scores[l[0]] = scores[l[1]]         
-            if guess == number:
-                scores[user] = guesses
-            values = scores.values()
-            values.sort(reverse=True)
-            print("*"*16+"SCOREBOARD"+"*"*16)
-            i = 0
-            for value in values:
-                i+=1
-                user = [u for u in scores if scores[u] == value]
-                print("{}. {}: {}".format(i, user[0], value))
-            print("*"*60)
-        while check != "y" or check != "yes":
-            check = input("Return to menu (y/n)? ")
-        menu()
-
 def menu():
     print("*"*16 + "WELCOME TO GUESS THE NUMBER"+"*"*16)
     print("""1. Play
 2. Rules
-3. Scoreboard
-4. Quit""")
+3. Quit""")
     print("*"*60)
-    select = int(input("Select an option(1-4): "))
+    select = int(input("Select an option(1-3): "))
     if select == 1:
         prompts = int(input("I would like a give up prompt every ... attempts: "))
         lLimit = int(input("minimum number: "))
@@ -98,8 +67,6 @@ def menu():
     if select == 2:
         rules()
     if select == 3:
-        scoreboard()
-    if select == 4:
         check = input("Are you sure (y/n)? ")
         if check == "y" or "yes":
             print("Thank you for playing.")
